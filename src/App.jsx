e0772c040666b6869a2254dc9531fc024804b759
tiff3b <tiffany3berger@gmail.com>
2026-02-02 09:27:50 -0500

    Added about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from '../components/Header'
+import About from '../components/About'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,8 @@ function App() {
   return (
     <>
       <Header
+        />
+      <About
         />
     </>
   )
