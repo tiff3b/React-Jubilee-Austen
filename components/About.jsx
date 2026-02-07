afd5c141b3db2589546a279c1c9729fc75dbc4a3
tiff3b <tiffany3berger@gmail.com>
2026-02-07 13:49:18 -0500

    Updated about styles

--- components/About.jsx
+++ components/About.jsx
@@ -3,6 +3,7 @@ export default function About (){
         <section id="about">
             <div class="content-wrapper">
                 <h2>A little bit about me.</h2>
+            <div class="about-info">
                 <p>My tech journey started with a degree in journalism. I loved researching, writing, and sharing my
                     voice online, but something felt missing. I wished for a way to share and display information
                     exactly the way I wanted. That's when I discovered and fell in love with coding and web design!
@@ -11,6 +12,7 @@ export default function About (){
                     still looking great. My goal with every site is to provide a top-of-the-line user experience.
                     Besides web development, I enjoy gardening and spending time with my small (but growing!) family.
                 </p>
+            </div>
             </div>
         </section>
     )
